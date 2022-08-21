8c5ebbaf9e1fcab4f183d38114efa58bddfaeeb1
MashaMartin <59993828+MashaMartin@users.noreply.github.com>
2022-08-21 20:10:26 +0200

    Add files via upload

---
+++ src/caseproject/caseclass.java
@@ -0,0 +1,23 @@
+package caseproject;
+
+public class caseclass {
+
+	public static void main(String[] args) {
+		// Switch condition
+		String string ="one";
+		switch (string) {
+		case "one":
+			System.out.println("Its 1");
+			break;
+		case "two":
+			System.out.println("Its 2");
+			break;
+		case "three":
+			System.out.println("Its 3 ");
+			break;
+		default:
+			System.out.println("Its not one of the numbers");
+		}
+	}
+
+}
